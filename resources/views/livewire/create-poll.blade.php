8defe903ef576b9af8c8a55d66f625e34702a9e8
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-20 20:34:54 -0300

    First Livewire Component

---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <form>
+        <label>
+            Poll Title
+        </label>
+
+
+        <input type="text" wire:model.live="title"/>
+
+        Current Title: {{$title}}
+    </form>
+</div>
