a06c878936d6d0ec57058739722a29171d5b6767
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-24 16:07:30 -0300

    Validation in Livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -10,6 +10,15 @@ class CreatePoll extends Component
 
     public $title;
     public $options = ['First'];
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255',
+    ];
+
+    protected $messages = [
+        "options.*" => "The option can't be empty."
+    ];
 
     public function render()
     {
@@ -27,8 +36,15 @@ class CreatePoll extends Component
         $this->options = array_values($this->options);
     }
 
+    public function updated($propertyName): void
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function createPoll(): void
     {
+        $this->validate();
+
         Poll::create([
             'title' => $this->title,
         ])->options()->createMany(
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -6,6 +6,9 @@
 
 
         <input type="text" wire:model.live="title"/>
+        @error('title')
+        <div class="text-red-500">{{$message}}</div>
+        @enderror
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">
@@ -20,8 +23,12 @@
 
                     <div class="flex gap-2">
                         <input type="text" wire:model="options.{{$index}}"/>
+
                         <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                    <div class="text-red-500">{{$message}}</div>
+                    @enderror
                 </div>
 
             @endforeach
