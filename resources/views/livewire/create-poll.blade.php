9b152268606322aed5d26a311d9f50a741b81b81
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-24 15:32:11 -0300

    Actions in Livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -8,5 +8,17 @@
         <input type="text" wire:model.live="title"/>
 
         Current Title: {{$title}}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">
+                Add Option
+            </button>
+        </div>
+
+        @foreach($options as $index => $option)
+            <div class="mb-4">
+                {{$index}} - {{$option}}
+            </div>
+        @endforeach
     </form>
 </div>
