3b543a9d09136cb775ece33a07267a7140d149c4
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-24 16:17:11 -0300

    Listing Polls Component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,13 +13,16 @@
         .btn {
             @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
         }
+
         label {
             @apply block uppercase text-slate-700 mb-2
         }
+
         input,
         textarea {
             @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
         }
+
         .error {
             @apply text-red-500 text-sm
         }
@@ -32,7 +35,16 @@
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
 @livewireScripts
 
-@livewire('create-poll')
+<div>
+    <h2 class="mb-4 mt-4 text-2xl">Create Poll</h2>
+    @livewire('create-poll')
+</div>
+
+<div>
+    <h2 class="mb-4 mt-4 text-2xl">Available Polls</h2>
+    @livewire('polls')
+</div>
+
 </body>
 
 </html>
---
+++ resources/views/livewire/polls.blade.php
@@ -0,0 +1,23 @@
+<div>
+   @forelse($polls as $poll)
+
+        <div class="mb-4 text-xl">
+
+            <h3>{{$poll->title}}</h3>
+
+        </div>
+
+
+        @foreach($poll->options as $option)
+            <div class="mb-2">
+                <button class="btn">Vote</button>
+                {{$option->name}} {{$option->votes->count()}}
+            </div>
+        @endforeach
+
+    @empty
+        <div class="text-gray-500">
+            No Polls Available!
+        </div>
+    @endforelse
+</div>
